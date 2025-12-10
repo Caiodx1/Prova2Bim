869452ca25ba172c29d5602c30a1995413abe7bc
Caiodx1 <71298735+Caiodx1@users.noreply.github.com>
2025-12-10 18:39:03 -0300

    crianca.cs

---
+++ crianca.cs
@@ -0,0 +1,13 @@
+ namespace dominio;
+
+public class Crianca
+{
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Cpf { get; set; }
+    public bool TemAlergia { get; set; }
+    public string DescricaoAlergia { get; set; }
+    public string NomeMae { get; set; }
+    public string TelefonePais { get; set; }
+}
